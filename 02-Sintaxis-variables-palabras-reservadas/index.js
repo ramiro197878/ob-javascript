5cdc1481681bef678e8d9108e8252b927b8b6e28
Ramiro Astete <24412705+ramiro197878@users.noreply.github.com>
2022-06-23 00:51:15 +0200

    02-Sintaxis, variables y palabras reservadas - ejercicio

---
+++ 02-Sintaxis-variables-palabras-reservadas/index.js
@@ -0,0 +1,13 @@
+const persona = {
+  nombre: "rama",
+  edad: 40,
+  isDeveloper: true,
+  "fecha-nacimiento": new Date(1, 2, 1981),
+  "libro-favorito": {
+    titulo: "El mundo de sofia",
+    autor: "Jostein Gaarder",
+    fecha: 1991,
+    url: "https://www.amazon.es/mundo-Sof%C3%ADa-Edades-Biblioteca-Gaarder/dp/8498414512"
+  }
+}
+console.log(persona["libro-favorito"]);
