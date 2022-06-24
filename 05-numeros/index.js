b2e13fbee381d346122275ef7a119a48398bb23d
Ramiro Astete <24412705+ramiro197878@users.noreply.github.com>
2022-06-25 00:11:40 +0200

    5 - Numeros - ejercicio

---
+++ 05-numeros/index.js
@@ -0,0 +1,14 @@
+let alturaCentimetros = 182;
+let alturaMetros = 1.82;
+let pesoKilogramos = 92.5;
+let alturaRedondeada = Math.ceil(alturaMetros);
+let pesoRedondeado = Math.floor(pesoKilogramos);
+let maximo = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
+
+console.log(alturaCentimetros);
+console.log(alturaMetros);
+console.log(pesoKilogramos);
+console.log(alturaRedondeada);
+console.log(pesoRedondeado);
+console.log(maximo);
