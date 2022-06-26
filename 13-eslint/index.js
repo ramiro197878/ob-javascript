b16ed05a733a5f5e7932af272a87ce1cafd03ae1
Ramiro Astete <24412705+ramiro197878@users.noreply.github.com>
2022-06-27 01:19:25 +0200

    13 - Eslint - ejercicio

---
+++ 13-eslint/index.js
@@ -0,0 +1,20 @@
+let nombre = "John"
+let apellido = "Doe"
+let estudiante = nombre.concat(" ", apellido)
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLocaleLowerCase()
+let contarEstudiante = estudiante.length
+let primeraLetra = nombre.charAt(0)
+let ultimaLetra = apellido.slice(-1)
+let eliminarEspacios = estudiante.replace(" ", "")
+let buscarNombre = estudiante.includes(nombre)
+
+
+console.log(estudiante)
+console.log(estudianteMayus)
+console.log(estudianteMinus)
+console.log(contarEstudiante)
+console.log(primeraLetra)
+console.log(ultimaLetra)
+console.log(eliminarEspacios)
+console.log(buscarNombre)
