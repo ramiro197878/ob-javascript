c239625889ac5a38202bcc3f16fc179c157a1600
Ramiro Astete <24412705+ramiro197878@users.noreply.github.com>
2022-06-27 11:44:19 +0200

    15 - Persistencia

---
+++ 15-persistencia/index.js
@@ -0,0 +1,12 @@
+const nombre = "Armando"
+const apellido = "Zamarbide"
+
+const datos = {nombre, apellido}
+
+// localStorage.setItem("datosLocal", JSON.stringify(datos))
+
+// sessionStorage.setItem("datosSession", JSON.stringify(datos))
+
+// let date = new Date()
+// date.setTime(date.getTime()+(2*60*1000))
+// document.cookie = "datosCookie="+JSON.stringify(datos)+",expires=" + date.toUTCString()
